b399951cc3884fd45b5a79970a74104fa7e6a7ef
claytonwashington <washington.534@osu.edu>
2023-10-18 12:21:27 -0400

    comments and data

--- data_exploration.py
+++ data_exploration.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %% Load data
-spike_clusters = np.load("../spike_clusters.npy")
-spike_times = np.load("../spike_times.npy")
+spike_clusters = np.load("data/spike_clusters.npy")
+spike_times = np.load("data/spike_times.npy")
 
 # %% Create a unique list of cluster IDs
 cluster_ids = np.unique(spike_clusters)
@@ -17,19 +17,26 @@ for i, cluster in enumerate(cluster_ids):
     cluster_times = spike_times[spike_clusters == cluster]
     trains_matrix[i, cluster_times] = 1
 
-# %% Plot the spike trains
-plt.imshow(trains_matrix[:,13311000:13312000], cmap=plt.cm.binary, aspect='auto', origin='lower')
+
+# %% Find region of dense spikes for initial plot (13311000)
+scale_factor = 1000
+# partial_sums = []
+# for i in range(int(np.ceil(len(trains_matrix[0])/scale_factor))):
+#     try:
+#         partial_sums.append(np.sum(trains_matrix[:,scale_factor*i:scale_factor*(i+1)]))
+#     except:
+#         partial_sums.append(np.sum(trains_matrix[:,scale"_factor*i:]))
+# dense_spike_start = scale_factor*np.argmax(partial_sums)
+
+dense_spike_start = 13311000 #for initial data
+
+
+# %% Plot the spike trains 
+plt.imshow(trains_matrix[:,dense_spike_start:dense_spike_start+scale_factor], cmap=plt.cm.binary, aspect='auto', origin='lower')
 plt.xlabel('Time')
 plt.ylabel('Cluster')
 plt.title('Spike Trains by Cluster')
 plt.show()
 
-# %%
-partial_sums = []
-for i in range(int(np.ceil(len(trains_matrix[0])/1000))):
-    try:
-        partial_sums.append(np.sum(trains_matrix[:,1000*i:1000*(i+1)]))
-    except:
-        partial_sums.append(np.sum(trains_matrix[:,1000*i:]))
-np.argmax(partial_sums)
+
 # %%
