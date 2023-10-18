8d0ce0abf6facabfd5cc7ecbb6b4a8f1ebcbbca0
Lahiru Wimalasena <lwimala@emory.edu>
2023-10-18 14:52:21 -0400

    update data_exploration script to incorporate sample rate to compute spike times. updated figure generation

--- data_exploration.py
+++ data_exploration.py
@@ -1,41 +1,51 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.cm as colormap
 # %% Load data
 spike_clusters = np.load("/snel/share/share/data/scpu_data/spike_clusters.npy")
-spike_times = np.load("/snel/share/share/data/scpu_data/spike_times.npy")
+spike_samp_nums = np.load("/snel/share/share/data/scpu_data/spike_times.npy")
+
+sampling_rate = 30000 # Hz
+ 
+spike_times = spike_samp_nums / sampling_rate
 
 # %% Create a unique list of cluster IDs
 cluster_ids = np.unique(spike_clusters)
 
 # %% Create an empty matrix to store the spike trains
-trains_matrix = np.zeros((len(cluster_ids), int(np.max(spike_times)) + 1))
+max_spike_time_s = int(np.max(spike_times))
+bin_size_s = 0.010 # 10 ms bins
+bins = np.arange(0, max_spike_time_s+ bin_size_s, bin_size_s) # 1 ms bins
+trains_matrix = np.zeros((bins.size-1, len(cluster_ids)))
 
 # %% Fill the matrix with spike train data
+n_spikes_per_cluster = []
 for i, cluster in enumerate(cluster_ids):
     cluster_times = spike_times[spike_clusters == cluster]
-    trains_matrix[i, cluster_times] = 1
-
-
-# %% Find region of dense spikes for initial plot (13311000)
-scale_factor = 1000
-# partial_sums = []
-# for i in range(int(np.ceil(len(trains_matrix[0])/scale_factor))):
-#     try:
-#         partial_sums.append(np.sum(trains_matrix[:,scale_factor*i:scale_factor*(i+1)]))
-#     except:
-#         partial_sums.append(np.sum(trains_matrix[:,scale"_factor*i:]))
-# dense_spike_start = scale_factor*np.argmax(partial_sums)
-
-dense_spike_start = 13311000 #for initial data
+    binned_spikes, _ = np.histogram(cluster_times, bins=bins)
+    trains_matrix[:, i] = binned_spikes
+    n_spikes_per_cluster.append(cluster_times.size)
 
 
 # %% Plot the spike trains 
-plt.imshow(trains_matrix[:,dense_spike_start:dense_spike_start+scale_factor], cmap=plt.cm.binary, aspect='auto', origin='lower')
-plt.xlabel('Time')
-plt.ylabel('Cluster')
-plt.title('Spike Trains by Cluster')
+
+# plot params
+plot_n_chans = 300 # number of channels to plot
+plot_len_s = 480 # seconds 
+
+plot_order = np.argsort(n_spikes_per_cluster)
+plot_chans = plot_order[-plot_n_chans:]
+plot_len = int(plot_len_s*(1/bin_size_s))
+fig = plt.figure(figsize=(10,4), dpi=200)
+ax = fig.add_subplot(111)
+ax.pcolor(trains_matrix[:plot_len, plot_chans].T, vmin=0, vmax=1, cmap=colormap.binary)
+ax.set_xticklabels(ax.get_xticks()*bin_size_s)
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Cluster (sorted)')
+ax.set_title('Spike Trains by Cluster')
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
 plt.show()
 
 
