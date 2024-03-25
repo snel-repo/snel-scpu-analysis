a1718ebc460c858cb88d092afa3700c8dd793a3a
Nathan Govindarajan <nathan.mail22@gmail.com>
2024-03-25 18:40:45 -0400

    add unwrap to angle. also added gaussian normalization to linear regression inputs. small cleanup in plot of how R^2 changes as samples increase

--- scripts/analysis/decoding_functions.py
+++ scripts/analysis/decoding_functions.py
@@ -10,7 +10,7 @@ from sklearn.metrics import r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
-
+SAMPLE_RATE = 500 # hertz
 """
 This function takes a numpy array and returns concatenated data based on start and stop indices
 """
@@ -45,7 +45,6 @@ def return_all_nonNan_slice(dataset, use_smooth_data: bool = True, use_LFADS: bo
     all_kin_data['Original_Index'] = np.arange(len(all_kin_data))
     all_kin_data_nonNan = all_kin_data.dropna().copy()
     non_Nan_indices = all_kin_data_nonNan['Original_Index'].values
-    print(non_Nan_indices)
     rates_slice = all_rates_data.iloc[non_Nan_indices]
     return all_kin_data_nonNan, rates_slice, kin_column_names
 
@@ -61,29 +60,33 @@ def linear_regression_train_val(x, y, alpha=0, folds=5):
     x is 2D array of shape (n_bins, n_channels) which is (samples, features)
     y is 1D array of shape (n_bins,) where each value is kinematic data
     """
-
+    from sklearn.preprocessing import StandardScaler
     kf = KFold(n_splits=folds,  shuffle=True, random_state=42)
     lr = Ridge(alpha=alpha)
 
     r2_test = np.zeros(folds)
     r2_train = np.zeros(folds)
-    fold_mean_x = [] # for test data
-    fold_mean_y = [] # for test data
-    test_pred = np.zeros_like(y)
 
+    test_pred = np.zeros_like(y)
+    scaler = StandardScaler()
     for split_num, (train_ix, test_ix) in enumerate(kf.split(x)):
         x_train, x_test = x[train_ix, :], x[test_ix, :]
         y_train, y_test = y[train_ix], y[test_ix]
 
+        # normalize predictor
+        x_train = scaler.fit_transform(x_train)
+        x_test = scaler.transform(x_test)
+
+        # normalize target
+        y_train = scaler.fit_transform(y_train.reshape(-1, 1)).ravel()
+        y_test = scaler.transform(y_test.reshape(-1, 1)).ravel()
 
         lr.fit(x_train, y_train)
         test_pred[test_ix] = lr.predict(x_test)
         train_pred = lr.predict(x_train)
         r2_test[split_num] = r2_score(y_test, test_pred[test_ix])
         r2_train[split_num] = r2_score(y_train, train_pred)
-        
-        fold_mean_x.append(np.mean(x_test,axis=0))
-        fold_mean_y.append(np.mean(y_test))
+
     
     test_sem = np.std(r2_test) / np.sqrt(folds)
     train_sem = np.std(r2_train) / np.sqrt(folds)
@@ -97,6 +100,7 @@ def linear_regression_train_val(x, y, alpha=0, folds=5):
 def append_kinematic_angle_data(all_kin_df: pd.DataFrame) -> pd.DataFrame:
     """
         Calculates and Appends knee and ankle angle data to kinematic data and returns all kin data and angles in one dataframe
+        Return angles are in degrees
     """
     # get knee and ankle angle data
     ankle_kinematics_np = all_kin_df[["ankle_x", "ankle_y"]].values
@@ -119,8 +123,16 @@ def append_kinematic_angle_data(all_kin_df: pd.DataFrame) -> pd.DataFrame:
     mag_ankle_to_toe = np.linalg.norm(ankle_to_toe_vectors, axis=1)
 
     # Angle calculation
-    knee_angles_vectorized = np.arccos(dot_product_knee / (mag_hip_to_knee * mag_knee_to_ankle)) * (180 / np.pi)
-    ankle_angles_vectorized = np.arccos(dot_product_ankle / (mag_knee_to_ankle * mag_ankle_to_toe)) * (180 / np.pi)
+    knee_angles_vectorized = np.arccos(dot_product_knee / (mag_hip_to_knee * mag_knee_to_ankle)) 
+    ankle_angles_vectorized = np.arccos(dot_product_ankle / (mag_knee_to_ankle * mag_ankle_to_toe))
+
+    # phase unwrapping for knee and ankle angles
+    knee_angles_vectorized = np.unwrap(knee_angles_vectorized)
+    ankle_angles_vectorized = np.unwrap(ankle_angles_vectorized)
+
+    # convert to degrees
+    knee_angles_vectorized = np.degrees(knee_angles_vectorized)
+    ankle_angles_vectorized = np.degrees(ankle_angles_vectorized)
 
     
     all_kin_df["knee_angle"] = knee_angles_vectorized
@@ -133,6 +145,9 @@ def plot_rates_vel_reg(regression_vel_slice, regression_rates_slice, orig_indice
     """
     Plots velocity and rates (input to linear regression model)
     """
+
+    assert len(orig_indices) == len(regression_vel_slice), "Length of original indices must be the same as the length of the data"
+    assert len(orig_indices) == regression_rates_slice.shape[0], "Length of original indices must be the same as the length of the data"
     fig, ax = plt.subplots(2, 1, figsize=(6, 10))
     
     fig.suptitle(f'Velocity and Rates indices {orig_indices[0], orig_indices[-1]}')
@@ -146,7 +161,7 @@ def plot_rates_vel_reg(regression_vel_slice, regression_rates_slice, orig_indice
     c = ax[1].pcolor(regression_rates_slice.T, cmap='viridis', vmin=0, vmax=vmax)
     ax[1].set_title('Rates')
     ax[1].set_xlabel('Time (bins)')
-    ax[1].set_ylabel('Rates')
+    ax[1].set_ylabel('Channels')
     plt.colorbar(c)
     plt.show()
 
@@ -164,7 +179,7 @@ def plot_psd_kinematic_data(kin_slice, use_smooth_data, bin_size=2):
     ax.set_ylabel('PSD [V**2/Hz]')
     plt.show()
 
-def preprocessing(dataset, column_name, use_smooth_data, start_indices, stop_indices, plot=False, use_LFADS=True):
+def preprocessing(dataset, column_name, use_smooth_data, start_indices, stop_indices, plot=False, use_LFADS=True, use_velocity=True):
     """
         preprocessing steps. output will be used for linear regression
     """
@@ -175,9 +190,22 @@ def preprocessing(dataset, column_name, use_smooth_data, start_indices, stop_ind
    
     all_kin_slice_and_angle_df = append_kinematic_angle_data(all_kin_df)
 
-    kin_slice = all_kin_slice_and_angle_df[column_name].values
-    regression_vel_slice = kin_slice
-    #regression_vel_slice = diff_filter(kin_slice)
+    regression_vel_slice = all_kin_slice_and_angle_df[column_name].values
+
+   
+
+    cutoff_freq = 5
+    nyquist = 0.5 * SAMPLE_RATE
+    normal_cutoff = cutoff_freq / nyquist
+    b, a = signal.butter(2, normal_cutoff, btype='low', analog=False)
+
+    if use_velocity:
+        regression_vel_slice = diff_filter(regression_vel_slice)
+
+    regression_vel_slice = signal.filtfilt(b, a, regression_vel_slice)
+
+
+
     regression_rates_slice = np.log(rates_slice + 1e-10)
 
     # subselect data from non NaN indices
@@ -191,18 +219,18 @@ def preprocessing(dataset, column_name, use_smooth_data, start_indices, stop_ind
         regression_rates_slice = concat_data_given_start_stop_indices(regression_rates_slice, start_indices, stop_indices)
         
         orig_indices = concat_data_given_start_stop_indices(all_kin_df['Original_Index'].values, start_indices, stop_indices)
-        print(f"Orig_indices: {orig_indices}")
-
-    index_smallest_10 = np.argpartition(regression_vel_slice, 10)[:10]
-    index_largest_10 = np.argpartition(regression_vel_slice, -10)[-10:]
-    outlier_indices = np.concatenate((index_smallest_10, index_largest_10))
+    else:
+        orig_indices = all_kin_df['Original_Index'].values
+    # index_smallest_10 = np.argpartition(regression_vel_slice, 10)[:10]
+    # index_largest_10 = np.argpartition(regression_vel_slice, -10)[-10:]
+    # outlier_indices = np.concatenate((index_smallest_10, index_largest_10))
     
     # outlier_min = np.argmin(regression_vel_slice)
     # val_min = regression_vel_slice[outlier_min]
     # print(val_min)
     # outlier_max = np.argmax(regression_vel_slice)
-    regression_vel_slice = np.delete(regression_vel_slice, outlier_indices)
-    regression_rates_slice = np.delete(regression_rates_slice, outlier_indices, axis=0)
+    # regression_vel_slice = np.delete(regression_vel_slice, outlier_indices)
+    # regression_rates_slice = np.delete(regression_rates_slice, outlier_indices, axis=0)
 
     # apply wiener filter
     # m = 20
--- scripts/analysis/decoding_notebook.py
+++ scripts/analysis/decoding_notebook.py
@@ -36,37 +36,39 @@ from sklearn.metrics import r2_score
 # Load everything
 
 # we use smoothed kinematic data for decoding so we don't have to smooth it every time
-load_smooth_kin_data = True
+load_smooth_data = True
 
 path_config, ld_cfg, merge_config = load_cfgs(yaml_config_path)
-smooth_kin_data_path = "/snel/share/share/derived/scpu_snel/nwb_lfads/runs/binsize_2/NG_smooth_kin_data.pkl"
+smooth_data_path = "/snel/share/share/derived/scpu_snel/nwb_lfads/runs/binsize_2/NG_smooth_data.pkl"
 yaml_config_path = "../configs/lfads_dataset_cfg.yaml"
 
-if load_smooth_kin_data:
-    with open(smooth_kin_data_path,'rb') as inf:
+if load_smooth_data:
+    with open(smooth_data_path,'rb') as inf:
         dataset = pkl.load(inf)
         bin_size = dataset.bin_size
 
         
 else:
+    gauss_width_ms = 25
 
     dataset, bin_size = load_dataset_and_binsize(yaml_config_path)
     dataset.smooth_spk(signal_type='kin_pos', gauss_width=3, name='smooth_3', overwrite=False)
-    with open(smooth_kin_data_path, "wb") as f:
+    dataset.smooth_spk(signal_type='spikes', gauss_width=gauss_width_ms, name='smooth_25', overwrite=False)
+
+    with open(smooth_data_path, "wb") as f:
         dill.dump(dataset, f, protocol=dill.HIGHEST_PROTOCOL, recurse=True)
 
     # dataset has kinematic data, smoothed spikes, and smoothed lfads_factors
 
 # %% 
 #dataset.smooth_spk(signal_type='kin_pos', gauss_width=20, name='smooth_20', overwrite=False)
-gauss_width_ms = 100
+gauss_width_ms = 25
 gauss_width_s = gauss_width_ms/1000
 # cutoff frequency using -3db cutoff
 cutoff_freq = (2 * np.sqrt(2 * np.log(2))) / (gauss_width_s*(np.sqrt(np.pi)))
 print(f'Cutoff frequency: {cutoff_freq} Hz')
 # %%
 #dataset.smooth_spk(signal_type='lfads_rates', gauss_width=gauss_width_ms, name=f'smooth_{gauss_width_ms}', overwrite=False)
-dataset.smooth_spk(signal_type='spikes', gauss_width=gauss_width_ms, name='smooth_25', overwrite=False)
 # dataset.smooth_spk(signal_type='lfads_factors', gauss_width=8, name='smooth_8', overwrite=False)
 # dataset.data.lfads_factors_smooth_8 = dataset.data.lfads_factors_smooth_8.fillna(0)
 
@@ -87,46 +89,31 @@ for index, trial in dataset.trial_info.iterrows():
 # %%
 use_smooth_data = False
 
-# fig, axs = plt.subplots(3, 2, figsize=(10, 12))
-# fig.suptitle(f'Kinematic Data vs Predicted Kinematic Data for different alphas and folds. Smoothed: {use_smooth_data}')
 
-# big_ax = fig.add_subplot(111, frameon=False)
-# # Hide tick and tick label of the big subplot
-# big_ax.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-# big_ax.grid(False)
-
-# # Set the labels
-# big_ax.set_xlabel('Time (bins)', labelpad=5)
-# big_ax.set_ylabel('Velocity', labelpad=0)
-
-
-# ax = axs.flatten()
-#alpha_values = [1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1, 10, 100, 1000, 10000]
 alpha_values = np.logspace(-5, 2, num=10)
-
+use_velocity = True
 
 r2_values_test_all = [] 
 r2_values_train_all = []
-train_sem_all = [] 
+train_sem_all = []
+
 test_sem_all = [] 
 fold = 10
 r2_test_value_fold = []
 r2_train_value_fold = []
 # these are handpicked indices from the nonNan data. currently, they are based on the first video
-start_indices = [0] # this is orig_index 58153 and first video starts at 58027 meaning we have kinematics from 
-stop_indices = [5000] # end times for video in trial_info is wrong so can just choose some arbitrary end time
-# start_idx = [9650]
-# stop_idx = [9650+600]
-# by itself, 9650 start index, 9650+600 stop index  R^2 = 0.748
-# by itself, 11000 start index, 11000+400 stop index R^2 = 0.871
-# together, R^2 = 0.578
-
+# start_indices = [0] # this is orig_index 58153 and first video starts at 58027 meaning we have kinematics from 
+# stop_indices = [5000] # end times for video in trial_info is wrong so can just choose some arbitrary end time
+start_indices = [18000]
+stop_indices = [22000+4000]
+# start_indices = []
+# stop_indices = []
 for idx, column_name in enumerate(dataset.data.kin_pos.columns):
     if idx > 0:
         # only decode one column for now
         break
-    column_name = "knee_angle"
-    regression_vel_slice, regression_rates_slice, orig_indices = preprocessing(dataset, column_name, use_smooth_data, start_indices=start_indices, stop_indices=stop_indices, plot=True, use_LFADS=True)
+    column_name = "ankle_x"
+    regression_vel_slice, regression_rates_slice, orig_indices = preprocessing(dataset, column_name, use_smooth_data, start_indices=start_indices, stop_indices=stop_indices, plot=True, use_LFADS=True, use_velocity=use_velocity)
     best_alpha = None
     best_r2 = -np.inf
     for i, alpha in enumerate(alpha_values):
@@ -162,14 +149,15 @@ for idx, column_name in enumerate(dataset.data.kin_pos.columns):
     # fig.subplots_adjust(wspace=0.5)
     # plt.show()
 
-
+# %%
+velocity_title_modifier = 'Velocity' if use_velocity else 'Position'
 
 # plot best alpha
 fig, ax = plt.subplots(1, 2, figsize=(10, 4))
-fig.suptitle(f"LFADS Rates indices: {orig_indices[0], orig_indices[-1]}")
+fig.suptitle(f"LFADS Rates indices: {orig_indices[0], orig_indices[-1]} decoding {column_name} {velocity_title_modifier}")
 ax[0].plot(best_true, label='True')
 ax[0].plot(best_pred, label='Predicted')
-ax[0].set_title(f'Predicted vs True. Best Alpha: {round(best_alpha, 3)} $R^2$: {round(best_r2, 6)}')
+ax[0].set_title(f'Predicted vs True. Best Alpha: {round(best_alpha, 3)} Best Validation $R^2$: {round(best_r2, 6)}')
 ax[0].set_xlabel('Time (bins)')
 ax[0].set_ylabel('Velocity')
 ax[0].spines['right'].set_visible(False)
@@ -207,13 +195,14 @@ plt.show()
 # block to visualize how R^2 between  as more samples are included
 #from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-use_smooth_data = True
-start_idx = [22000]
-stop_indices = [[22000+400], [22000+600], [22000+800], [22000+1000], [22000+1200], [22000+1400], [22000+1600], [22000+1800], [22000+2000]]
+use_smooth_data = False
+use_velocity = True
+start_indices = [[22000], [22000], [22000], [22000], [22000], [22000], [22000], [22000], [22000], [22000], [22000, fill_in]]
+stop_indices = [[22000+600],  [22000+1000],  [22000+1400],  [22000+1800], [22000+2200], [22000 + 2600], [22000 + 3000], [22000 + 3400], [22000 + 3800], [22000 + 4200], [22000 + 4600, fill_in]]
 mean_kin_for_stop_indices = []
 var_kin_for_stop_indices = []
 num_batches = len(stop_indices)
-num_channels = regression_rates_slice.shape[1]
+num_channels = dataset.data.spikes.shape[1]
 mean_rates_for_stop_indices = np.zeros((num_batches, num_channels)) # mean of rates for each channel as more samples are included
 var_rates_for_stop_indices = np.zeros((num_batches, num_channels))  # variance of rates for each channel as more samples are included
 dataset_sizes = [stop_idx[0] - start_idx[0] for stop_idx in stop_indices]
@@ -235,7 +224,7 @@ for use_lfads_param in [True, False]:
         stop_idx = stop_indices[i]
         sample_size = dataset_sizes[i]
         
-        regression_vel_slice, regression_rates_slice = preprocessing(column_name, use_smooth_data=use_smooth_data, start_idx=start_idx, stop_idx=stop_idx, plot=False, use_LFADS=use_lfads_param)
+        regression_vel_slice, regression_rates_slice, orig_indices = preprocessing(dataset, column_name, use_smooth_data=use_smooth_data, start_indices=start_idx, stop_indices=stop_idx, plot=False, use_LFADS=use_lfads_param, use_velocity=use_velocity)
 
         for idx, alpha_x in enumerate(alpha_values):
 
@@ -250,7 +239,7 @@ for use_lfads_param in [True, False]:
             # var_rates_for_stop_indices[i, :] = np.var(regression_rates_slice, axis=0)
             if r2_test > best_r2:
                 best_r2 = r2_test
-                best_alpha = alpha
+                best_alpha = alpha_x
         if use_lfads_param:
             r2_across_dataset_sizes_LFADS.append(best_r2)
         else:
@@ -258,46 +247,25 @@ for use_lfads_param in [True, False]:
 
 assert(len(r2_across_dataset_sizes_smooth) == len(stop_indices))
 assert(len(r2_across_dataset_sizes_LFADS) == len(stop_indices))
+# %% 
+velocity_title_modifier = 'Velocity' if use_velocity else 'Position'
+plt.figure()
+plt.title(f'Change in Validation $R^2$ as more samples are included decoding {column_name} {velocity_title_modifier}\n$R^2$ chosen from L2 regularization sweep')
+plt.plot(dataset_sizes, r2_across_dataset_sizes_smooth, '-o', label='Smoothed $R^2$')
+plt.plot(dataset_sizes, r2_across_dataset_sizes_LFADS, '-o', label='LFADS R^2')
+plt.xticks(dataset_sizes)
+plt.xlabel('Sample Size')
+plt.ylabel('$R^2$')
+plt.legend()
 
-fig, axs = plt.subplots(1, 2, figsize=(10, 4))
-fig.suptitle(f'Change in R^2 as more samples are included')
-axs[0].plot(dataset_sizes, r2_across_dataset_sizes_smooth, '-o', label='Smoothed R^2')
-axs[0].set_title('Smoothed R^2')
-axs[0].set_xlabel('Sample Size')
-axs[0].set_ylabel('R^2')
-axs[0].legend()
-axs[1].plot(dataset_sizes, r2_across_dataset_sizes_LFADS, '-o', label='LFADS R^2')
-axs[1].set_title('LFADS R^2')
-axs[1].set_xlabel('Sample Size')
-axs[1].set_ylabel('R^2')
-axs[1].legend()
+
 plt.show()
 
 
 
-# # Flatten the sample sizes and channels to use in scatter plot
-# x_indices = np.repeat(np.arange(num_batches), num_channels)
-# y_indices = np.tile(np.arange(num_channels), num_batches)
+# %%
 
-# # Flatten the data array for color coding
-# colors = mean_rates_for_stop_indices.flatten()
-# # Setting labels and title
-# xticks_array = np.array(dataset_sizes).flatten()
-# rates_fig, ax = plt.subplots(1, 2, figsize=(10, 4))
-# rates_fig.suptitle(f'Statistics of rates for different stop indices')
-# ax[0].set_title('Mean Rates')
-# ax[0].set_xlabel('Sample Size')
-# ax[0].set_ylabel('Channels')
-# ax[0].set_xticks(ticks=np.linspace(0, num_batches-1, len(xticks_array)))
-# ax[1].set_title('Variance of Rates')
-# ax[1].set_xlabel('Sample Size')
-# ax[1].set_ylabel('Channels')
-# ax[1].set_xticks(ticks=np.linspace(0, num_batches-1, len(xticks_array)))
-# ax[0].scatter(x_indices, y_indices, c=mean_rates_for_stop_indices.flatten(), cmap='viridis')
-# ax[1].scatter(x_indices, y_indices, c=var_rates_for_stop_indices.flatten(), cmap='viridis')
-# rates_fig.colorbar(cm.ScalarMappable(cmap='viridis'), ax=ax[0], label='Mean Rates') # normalized color bar
-# rates_fig.colorbar(cm.ScalarMappable(cmap='viridis'), ax=ax[1], label='Variance of Rates')
-# plt.show()
+
 
 
 # %%
@@ -388,5 +356,18 @@ fig.subplots_adjust(hspace=0.5)
 
 plt.show()
 
+
+# %%
+# plot ankle_x position over all time
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 4))
+ankle_x_pos = dataset.data.kin_info_cw['mtarsal_x']
+ankle_x_pos = ankle_x_pos.fillna(-1)
+ankle_x_pos.index = np.arange(ankle_x_pos.shape[0])
+
+ankle_x_pos[:500000].plot(ax=ax)
+ax.set_title('Ankle X position over all time')
+ax.set_xlabel('Time (bins)')
+ax.set_ylabel('Position (au)')
 
 # %%
